97e763482a51b7679b88c81d7ce3498b656549a4
swathy s <swathysubu11@gmail.com>
2022-07-31 17:01:58 +0530

    collector pages connected

--- server/public/collector/orders/orders.js
+++ server/public/collector/orders/orders.js
@@ -3,6 +3,8 @@ let deny_btn=document.getElementById('deny-btn');
 let to_be_cnfm_orders_div=document.getElementById('to-be-cnfm-orders');
 let cnfrmed_orders_div=document.getElementsByClassName('confirmed-div')
 let acnt_link=document.getElementById('account-link');
+let order_link=document.getElementById('order-link');
+
 
 deny_btn.addEventListener('click',()=>{
     to_be_cnfm_orders_div.style.display="none"
@@ -13,5 +15,9 @@ accept_btn.addEventListener('click',()=>{
 })
 
 acnt_link.addEventListener('click',()=>{
-    window.location.href="cAcnt.html"
+    window.location.href="http://localhost:5000/collector/cAcnt/cAcnt.html"
+})
+
+order_link.addEventListener('click',()=>{
+    window.location.href="http://localhost:5000/collector/orders/orders.html"
 })
