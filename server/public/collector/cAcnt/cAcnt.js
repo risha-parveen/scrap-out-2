4e8b7129addde20a4cccc8c75788a1a34eaee98b
risha-parveen <rishaparveen12@gmail.com>
2022-08-01 11:15:12 +0530

    update

--- server/public/collector/cAcnt/cAcnt.js
+++ server/public/collector/cAcnt/cAcnt.js
@@ -1,9 +1,19 @@
+
+
 let create_shop_button=document.getElementById('create-shop-button');
 let shop_info=document.getElementById('shop-info-id');
 let first_shop_info=document.getElementById('new-shop-info');
 let order_link=document.getElementById('order-link');
 let view_shop_button=document.getElementById('view-shop-button');
 let acnt_link=document.getElementById('account-link');
+
+
+let token=null
+
+accountDetailsWrapper=document.getElementsByClassName('acnt-details-wrapper')
+categoryInfo=document.getElementsByClassName('category-info')
+
+console.log(categoryInfo[0].innerHTML)
 
 
 order_link.addEventListener('click',()=>{
@@ -13,24 +23,101 @@ order_link.addEventListener('click',()=>{
 acnt_link.addEventListener('click',()=>{
     window.location.href="http://localhost:5000/collector/cAcnt/cAcnt.html"
 })
-/*
-const checkLocalStorage=async ()=>{
-    created=localStorage.getItem('created')
-    if(created){
-        console.log("created")
-      create_shop_button.style.display="none"
-      view_shop_button.style.display=""
 
-    }else{
-        view_shop_button.style.display="none"
-        create_shop_button.style.display=""
+
+const renderData=async()=>{
+    response=await collectorInfo(token)
+    console.log(response)
+    personalNode=`
+    <div class="personal-info">
+        <h2 id="profile-tag">Profile</h2>
+        <ul class="shop-details">
+        <li>${response.shopname}</li>
+        <li>${response.email} </li>
+        <li>${response.phone} </li>
+        <li>${response.address}</li>
+        </ul>
+    </div>
+    `
+    accountDetailsWrapper[0].innerHTML+=personalNode
+
+    product_response=await getProducts(token)
+    console.log(product_response)
+
+    const eachItem=(items)=>{
+        node=''
+        for(let i=0;i<items.length;i++){
+            item=Object.keys(items[i])[0]
+            price=items[i][item]
+            node+=`<li>${item} : ${price} /kg</li>`
+        }
+        console.log(node)
+        return node
     }
-  }
-create_shop_button.addEventListener('click',()=>{
-    window.location.href="createShop.html"
-})
 
-view_shop_button.addEventListener('click',()=>{
-    window.location.href="cshop.html"
-})
-*/
+    itemNode=''
+    for(let i in product_response){
+        if(i==='shopname' || i==='__v' || i==='_id') continue
+
+        itemNode+=`
+            <h3>${i}</h3>
+            <ul>
+                ${eachItem(product_response[i])}
+            </ul>
+        `
+    }
+    categoryInfo[0].innerHTML+=itemNode
+    
+    
+    
+}
+
+const checklocalstorage=()=>{
+    token=localStorage.getItem('token')
+    if(!token){
+        window.location.href="http://localhost:5000/collector/clogin/clogin.html"
+    }
+    else{
+        renderData()
+    }
+}
+
+const collectorInfo=async(token)=>{
+    try{
+        const response=await fetch('/collector/get_account',{
+          method:'GET',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json',
+                'Authorization':'Bearer '+token
+            }
+        })
+        const result=await response.json()
+        console.log(result)
+        return result
+      }
+      catch(e){
+        console.log(e)
+      }
+}
+
+const getProducts=async(token)=>{
+    try{
+        const response=await fetch('/collector/get_products',{
+          method:'GET',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json',
+                'Authorization':'Bearer '+token
+            }
+        })
+        const result=await response.json()
+        console.log(result)
+        return result
+      }
+      catch(e){
+        console.log(e)
+      }
+}
+
+window.onload=checklocalstorage()
